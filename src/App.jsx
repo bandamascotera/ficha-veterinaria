19b99faa509f2b1d0d68653be0d0292574ff932f
Pablo Apaza <hunterpablo1@gmail.com>
2026-03-10 12:19:16 -0300

    login + pagina propietario 3

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import { supabase } from "./supabaseClient"
 import { useEffect } from "react"
 import PortalCliente from "./PortalCliente"
 
-"comentario"
 
 export default function App() {
   const [vista, setVista] = useState("inicio");
